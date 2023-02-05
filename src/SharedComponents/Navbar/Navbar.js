9622b3e2ba40adb5193d5991625e7f105c8b099e
Abhinav Agarwal <77193671+Abhinav-Agarwal021@users.noreply.github.com>
2023-02-05 23:43:28 +0530

    navbar changes

--- src/SharedComponents/Navbar/Navbar.js
+++ src/SharedComponents/Navbar/Navbar.js
@@ -1,53 +1,52 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './Navbar.module.css'
 import { Link } from "react-router-dom"
 
 export const Navbar = () => {
 
-    const [searchExp, setSearchExp] = useState("")
-
-    const handleSearch = () => {
-
-    }
-
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            handleSearch();
-        }
-    }
-
     return (
         <nav className={`${styles.navbar} container`}>
-            <div className={styles.brandStyle} to="/">
-                <img className={styles.logo} alt="" src="/Images/Indian_Institute_of_Technology_Roorkee_logo.png"></img>
-                <span className={styles.logoText}>
-                    <span className={styles.logoTextHindi}>
-                        <span>भारतीय</span>
-                        <span>प्रौद्योगिकी</span>
-                        <span>संस्थान</span>
-                        <span>रुड़की</span>
+            <div className={styles.logos}>
+                <img className={styles.eLogo} alt="" src="/Images/Elabslogo.png" />
+                <hr className={styles.logoBreaker} />
+                <div className={styles.brandStyle}>
+                    <img className={styles.logo} alt="" src="/Images/Indian_Institute_of_Technology_Roorkee_logo.png"></img>
+                    <span className={styles.logoText}>
+                        <span className={styles.logoTextHindi}>
+                            <span>भारतीय</span>
+                            <span>प्रौद्योगिकी</span>
+                            <span>संस्थान</span>
+                            <span>रुड़की</span>
+                        </span>
+                        <span className={styles.logoTextEng}>Indian Instiute of Technology Roorkee</span>
                     </span>
-                    <span className={styles.logoTextEng}>Indian Instiute of Technology Roorkee</span>
-                </span>
+                </div>
             </div>
             <hr className={styles.logoLineBreak} />
             <div className={styles.navMenu}>
-                <div className={styles.menuBtns}>
+                <div className={styles.leftMenu}>
                     <Link to="/">
                         <button className={styles.btnss}>
                             Home
                         </button>
                     </Link>
                     <button className={styles.btnss}>
+                        About
+                    </button>
+                    <button className={styles.btnss}>
+                        Exeriments
+                    </button>
+                    <button className={styles.btnss}>
                         Contact Us
                     </button>
+                </div>
+                <div className={styles.rightMenu}>
                     <button className={styles.btnss}>
                         Login
                     </button>
-                </div>
-                <div className={styles.searchBox}>
-                    <input type="text" value={searchExp} placeholder='Search for an experiment' className={styles.searchInput} onChange={(e) => setSearchExp(e.target.value)} onKeyDown={handleKeyDown} />
-                    <img src="/images/search-icon.png" alt="search" onClick={handleSearch} />
+                    <button className={styles.btnss}>
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </nav>
